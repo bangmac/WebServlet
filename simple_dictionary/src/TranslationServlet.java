cb90beaaa1d0a3902a0c1f21a1b88e75367913f1
bangmac <anhbexd@gmail.com>
2020-06-01 15:24:06 +0700

    add get method

--- simple_dictionary/src/TranslationServlet.java
+++ simple_dictionary/src/TranslationServlet.java
@@ -12,6 +12,29 @@ import java.util.Map;
 public class TranslationServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        Map<String, String> dictionary = new HashMap<>();
+//        dictionary.put("hello", "Xin chào");
+//        dictionary.put("how", "Thế nào");
+//        dictionary.put("book", "Quyển vở");
+//        dictionary.put("computer", "Máy tính");
+//
+//        String search = request.getParameter("txtSearch");
+//
+//        PrintWriter writer = response.getWriter();
+//        writer.println("<html>");
+//
+//        String result = dictionary.get(search);
+//        if(result != null){
+//            writer.println("Word: " + search);
+//            writer.println("Result: " + result);
+//        } else {
+//            writer.println("Not found");
+//        }
+//
+//        writer.println("</html>");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Map<String, String> dictionary = new HashMap<>();
         dictionary.put("hello", "Xin chào");
         dictionary.put("how", "Thế nào");
@@ -32,9 +55,5 @@ public class TranslationServlet extends HttpServlet {
         }
 
         writer.println("</html>");
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
 }
