a858928ce8f94f95aaff82aede9e36883fac5e98
bangmac <anhbexd@gmail.com>
2020-06-01 13:17:35 +0700

    test action method post and get

--- simple-login/src/LoginServlet.java
+++ simple-login/src/LoginServlet.java
@@ -6,9 +6,24 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "LoginServlet")
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        String username = request.getParameter("username");
+//        String password = request.getParameter("password");
+//
+//        PrintWriter writer = response.getWriter();
+//        writer.println("<html>");
+//
+//        if("admin".equals(username) && "admin".equals(password)) {
+//            writer.println("<h1>Welcome "+username+" to website</h1>");
+//        } else{
+//            writer.println("<h1>Login Error</h1>");
+//        }
+//        writer.println("</html>");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
@@ -20,11 +35,8 @@ public class LoginServlet extends HttpServlet {
         } else{
             writer.println("<h1>Login Error</h1>");
         }
-
         writer.println("</html>");
     }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 }
+
+
--- simple-login/web/index.jsp
+++ simple-login/web/index.jsp
@@ -6,21 +6,24 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<!DOCTYPE HTML>
 <html>
-<style type="text/css">
-  .login {
-    height:180px; width:230px;
-    margin:0;
-    padding:10px;
-    border:1px #CCC solid;
-  }
-  .login input {
-    padding:5px; margin:5px
-  }
-</style>
+<head>
+  <title>Login Page</title>
+  <style type="text/css">
+    .login {
+      height:180px; width:230px;
+      margin:0;
+      padding:10px;
+      border:1px #CCC solid;
+    }
+    .login input {
+      padding:5px; margin:5px
+    }
+  </style>
+</head>
 <body>
-<form method="post" action="/login">
+<form method="get" action="/login">
+<%--  <form method="get" action="/login">--%>
   <div class="login">
     <h2>Login</h2>
     <input type="text" name="username" size="30"  placeholder="username" />
