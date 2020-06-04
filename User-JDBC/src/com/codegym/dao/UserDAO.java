2fd3fcc52a226cfc19dbc85b60729e74a557c1f5
bangmac <anhbexd@gmail.com>
2020-06-04 15:35:32 +0700

    add note

--- User-JDBC/src/com/codegym/dao/UserDAO.java
+++ User-JDBC/src/com/codegym/dao/UserDAO.java
@@ -11,13 +11,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDAO implements IUserDAO{
-    private String jdbcURL = "jdbc:mysql://localhost:3306/demo?useSSL=false";
+    private String jdbcURL = "jdbc:mysql://localhost:3306/demo";
     private String jdbcUsername = "root";
     private String jdbcPassword = "password";
 
-    private static final String INSERT_USERS_SQL = "INSERT INTO users" + "  (name, email, country) VALUES " +
-            " (?, ?, ?);";
-
+    private static final String INSERT_USERS_SQL = "INSERT INTO users" + "  (name, email, country) VALUES " + " (?, ?, ?);";
     private static final String SELECT_USER_BY_ID = "select id,name,email,country from users where id =?";
     private static final String SELECT_ALL_USERS = "select * from users";
     private static final String DELETE_USERS_SQL = "delete from users where id = ?;";
@@ -27,10 +25,14 @@ public class UserDAO implements IUserDAO{
     }
 
     protected Connection getConnection() {
+        // tạo kết nối tới database
         Connection connection = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
+            // khai báo cho Driver biết thư viện mysql-connector nằm ở đâu
+            // với bản 8.0 trở nên sẽ là com.mysql.cj.jdbc.Driver
             connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+            // gán kết nối vào đối tượng connection thông qua DriverManager
         } catch (SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -41,15 +43,22 @@ public class UserDAO implements IUserDAO{
         return connection;
     }
 
-    public void insertUser(User user) throws SQLException {
+    public void insertUser(User user) {
+
         System.out.println(INSERT_USERS_SQL);
         // try-with-resource statement will auto close the connection.
-        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL))
+                // thực thi câu để thêm dữ liêu vào database
+                // vì là thêm chi tiết cả id và giá trị nên ta sẽ dùng preparedStatement thay vì dùng Statement
+        {
             preparedStatement.setString(1, user.getName());
             preparedStatement.setString(2, user.getEmail());
             preparedStatement.setString(3, user.getCountry());
+            // lấy dữ liệu từ người dùng nhập vào để thiết lập giá trị cho từng trường để đẩy vào database
             System.out.println(preparedStatement);
             preparedStatement.executeUpdate();
+            // thực thi câu lệnh để thi hành việc cập nhật những dữ liệu trên vào database
         } catch (SQLException e) {
             printSQLException(e);
         }
@@ -80,20 +89,18 @@ public class UserDAO implements IUserDAO{
     }
 
     public List<User> selectAllUsers() {
-
         // using try-with-resources to avoid closing resources (boiler plate code)
         List<User> users = new ArrayList<>();
         // Step 1: Establishing a Connection
         try (Connection connection = getConnection();
-
              // Step 2:Create a statement using connection object
              PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_USERS);) {
             System.out.println(preparedStatement);
             // Step 3: Execute the query or update query
             ResultSet rs = preparedStatement.executeQuery();
-
             // Step 4: Process the ResultSet object.
             while (rs.next()) {
+                // duyệt next từng phần tử đc lấy ra từ resultSet sau đó thêm vào mảng
                 int id = rs.getInt("id");
                 String name = rs.getString("name");
                 String email = rs.getString("email");
@@ -108,7 +115,8 @@ public class UserDAO implements IUserDAO{
 
     public boolean deleteUser(int id) throws SQLException {
         boolean rowDeleted;
-        try (Connection connection = getConnection(); PreparedStatement statement = connection.prepareStatement(DELETE_USERS_SQL);) {
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(DELETE_USERS_SQL);) {
             statement.setInt(1, id);
             rowDeleted = statement.executeUpdate() > 0;
         }
